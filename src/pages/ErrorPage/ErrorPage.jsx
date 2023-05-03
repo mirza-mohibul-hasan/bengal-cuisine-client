52bfa9a7e6eb60bc8a92a966159e0de5b464b7fe
Mirza Mohibul Hasan <mirzamohibul618@.com>
2023-05-04 02:46:59 +0600

    Home Page Added

--- src/pages/ErrorPage/ErrorPage.jsx
+++ src/pages/ErrorPage/ErrorPage.jsx
@@ -5,9 +5,10 @@ import { Link, useRouteError } from 'react-router-dom'
 const ErrorPage = () => {
   const { error, status } = useRouteError()
   return (
-    <section className='flex items-center h-screen p-16 bg-gray-100 text-gray-900'>
+    <section className='flex items-center h-screen p-16 text-gray-900'>
       <div className='container flex flex-col items-center justify-center px-5 mx-auto my-8'>
-        <svg
+        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRvuDcciMwMZHVLJY1L7N8DAq06MgEBZ4FZhnTaKPMhD6GP4U4S8Cf9XVnfi2djcfsD-wM&usqp=CAU" alt="" />
+        {/* <svg
           xmlns='http://www.w3.org/2000/svg'
           viewBox='0 0 512 512'
           className='w-40 h-40 text-gray-600'
@@ -31,17 +32,17 @@ const ErrorPage = () => {
             fill='currentColor'
             points='383.958 182.63 360.042 161.37 338.671 185.412 314.63 164.042 293.37 187.958 317.412 209.329 296.042 233.37 319.958 254.63 341.329 230.588 365.37 251.958 386.63 228.042 362.588 206.671 383.958 182.63'
           ></polygon>
-        </svg>
+        </svg> */}
         <div className='max-w-md text-center'>
-          <h2 className='mb-8 font-extrabold text-9xl text-gray-600'>
-            <span className='sr-only'>Error</span> {status || 404}
+          <h2 className='mb-8 font-extrabold text-9xl text-red-700'>
+            <span className='sr-only '>Error</span> {status || 404}
           </h2>
           <p className='text-2xl font-semibold md:text-3xl mb-8'>
             {error?.message}
           </p>
           <Link
             to='/'
-            className='px-8 py-3 font-semibold rounded bg-cyan-200 text-gray-900'
+            className='px-8 py-3 font-semibold rounded bg-green-500 text-white'
           >
             Back to homepage
           </Link>
