31453e34bdba5bb8bc63d615f1ca2574907fc10d
Mirza Mohibul Hasan <mirzamohibul618@.com>
2023-05-03 16:14:53 +0600

    banner added

---
+++ src/pages/ChefCard/ChefCard.jsx
@@ -0,0 +1,14 @@
+/* eslint-disable no-unused-vars */
+import React from 'react';
+
+const ChefCard = (chef) => {
+    const {id, chefName} = chef.chef;
+    console.log(chefName)
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default ChefCard;
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,11 +1,28 @@
 /* eslint-disable no-unused-vars */
 import React from 'react';
+import { useLoaderData } from 'react-router-dom';
+import ChefCard from '../ChefCard/ChefCard';
 
 const Home = () => {
+    const chefs = useLoaderData();
+    // console.log(chefs);
     return (
-        <div>
-            <h2>This is home</h2>
-        </div>
+        <>
+            <div className='mt-4 mx-3 md:mx-56 md:flex items-center p-5' style={{ borderTop: '2px solid yellow' }}>
+                <div>
+                    <h1 className='text-5xl font-bold my-3'>One Step <br />Closer To Your <br /><span className='text-amber-500'>Daily Cooking</span></h1>
+                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Similique, excepturi repellendus temporibus ea assumenda magnam fuga corporis quod cumque porro?</p>
+                </div>
+                <div>
+                    <img className='rounded-2xl' src='https://www.denbypottery.com/file/v8807526844468382865/general/LIC-Headers-mobile-Food.jpg' alt="" />
+                </div>
+            </div>
+            <div>
+                {
+                    chefs.map(chef => <ChefCard key={chef.id} chef = {chef}></ChefCard>)
+                }
+            </div>
+        </>
     );
 };
 
